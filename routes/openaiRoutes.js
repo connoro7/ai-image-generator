66b87d795abd64ccb2000e1a3d807c077ebff9d5
Connor Dillon <connor@codewithconnor.com>
2022-12-07 19:54:52 -0800

    :bricks: Init basic  openAI routing

---
+++ routes/openaiRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+
+router.post('/generateimage', (req, res) => {
+  res.status(200).json({ success: true })
+})
+
+module.exports = router
