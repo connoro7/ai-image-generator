137b9e2ac125d21e74449bef0e65fcd63aed254e
Connor Dillon <connor@codewithconnor.com>
2022-12-09 16:11:27 -0800

    :goal_net: Fixed bad API response format on errors

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -26,9 +26,15 @@ const generateimage = async (req, res) => {
       data: imageUrl,
     })
   } catch (error) {
+    if (error.response) {
+      console.log(error.response.status)
+      console.log(error.response.data)
+    } else {
+      console.log(error.message)
+    }
     res.status(400).json({
       success: false,
-      error: error.message,
+      error: 'The image could not be generated at openaiController.js/generateImage()',
     })
   }
 }
