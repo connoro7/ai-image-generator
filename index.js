ee4ee873400d5376a1e12307fe372871d022765b
Connor Dillon <connor@codewithconnor.com>
2022-12-11 13:55:06 -0800

    :bricks: Added server middleware to serve static html to client

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 3000
@@ -9,6 +10,10 @@ app.use(express.json())
 // When testing with HTTP/API client, be sure to use "Form URL-Encoded" instead of "JSON" in body payload
 app.use(express.urlencoded({ extended: false }))
 
+// Middleware to serve static files on frontend
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Routes
 app.use('/openai', require('./routes/openaiRoutes'))
 
 app.listen(port, () => {
